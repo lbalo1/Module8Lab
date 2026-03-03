ed33e9ce14d073cf1792319f1ef057d47135a2c5
lbalo1 <ph5212qx@go.minnstate.edu>
2026-03-03 19:08:21 +0000

    Module 8 Lab: Create pokemon class

---
+++ Module8Lab/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualBasic;
+
+public class Pokemon
+{
+    // Attributes - these are varibles that hold values 
+    public int Id { get; set; }
+    // Adding a ? to make the attribute nullable
+    public string? Name { get; set; }
+    public string? Type { get; set; }
+    public int Level { get; set; }
+
+} // end pokemon class
+
+// Pokemon link lab class contains are main method
+// This is the first method that will be called when the program runs
+
+public class PokemonLinqLab
+{
+    public static void Main()
+    {
+        var pokemons = new List<Pokemon>
+        {
+            new Pokemon { Id = 1, Name = "Bulbasaur", Type = "Grass", Level = 15 },
+            new Pokemon { Id = 2, Name = "Charmander", Type = "Fire", Level = 36 },
+            new Pokemon { Id = 3, Name = "Squirtle", Type = "Water", Level = 10 },
+            new Pokemon { Id = 4, Name = "Pikachu", Type = "Electric", Level = 22 },
+            new Pokemon { Id = 5, Name = "Eevee", Type = "Normal", Level = 25 }
+        };
+
+        // Linq query: Find Pokemant that are ready to evolve
+
+        var readyToEvolve = from p in pokemons
+                            where p.Level >= 16
+                            orderby p.Level
+                            select p;
+
+        // Execute the query and display the results
+        Console.WriteLine("Pokemon Ready to Evolve:");
+        foreach (var p in readyToEvolve)
+        {
+            Console.WriteLine($"Name: {p.Name}, Type: {p.Type}, Level: {p.Level}");
+        }
+    }
+}// end class
+
+
+
+
